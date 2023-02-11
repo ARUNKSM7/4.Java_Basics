f0e1b84c6fba386c24db514dc80e24875b3cbb0e
ARUNKSM7 <arunksm7@gmail.com>
2023-02-11 18:33:41 +0530

    Swapping Two Numbers

---
+++ Swapping.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Swapping
+{
+public static void main(String[]args)
+{
+int a=10,b=5,c=0;
+c=a;
+a=b;
+b=c;
+System.out.println("The swapped value of a is:"+a);
+System.out.println("The swapped value of b is:"+b);
+}
+}
